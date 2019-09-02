06cd6a7cb01629e591aadca010f7d1ae9ec8a709
Zeindelf <zeindelf@hotmail.com>
2019-09-02 00:08:40 -0300

    feat: init CMS methods

--- src/VtexCMS.js
+++ src/VtexCMS.js
@@ -1,16 +1,73 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 class VtexCMS {
-  constructor() {
-    this.foo = 'VTEX CMS Class';
-    this.token = 'TOKEN';
+  setAccount(account, authCookie) {
+    this.account = account;
+    this.authCookie = authCookie;
+    this.uri = `https://${account}.vtexcommercestable.com.br`;
+    this.api = axios.create({
+      baseURL: this.uri,
+      headers: {
+        'Cache-Control': 'no-cache',
+        Accept: '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      timeout: 10000,
+    });
   }
 
-  download() {
-    return this.foo;
+  async getRequestToken() {
+    this.api.interceptors.request.use((config) => {
+      config.headers.post['Cookie'] = `VtexIdclientAutCookie=${this.authCookie};`; // eslint-disable-line
+      return config;
+    });
+
+    const { data } = await this.api.post('/admin/a/PortalManagement/AddFile?fileType=js');
+    const $ = cheerio.load(data);
+    const requestToken = $('#fileUploadRequestToken').val();
+
+    if (!requestToken) {
+      throw new Error('Couldn\'t get request token!');
+    }
+
+    this.requestToken = requestToken;
   }
 
-  upload(files = '') {
-    this.token = files;
-    return this.token;
+  async getLegacyTemplateId(templateName, type, isSub = false) {
+    const templatesList = await this.getTemplates(type, isSub);
+    const regex = new RegExp(`(${templateName})([\\s\\S]+?)(templateId=)([\\s\\S]+?(?="))`);
+    const templateMatch = templatesList.match(regex);
+
+    if (!templateMatch) {
+      throw new Error('Template not found');
+    }
+
+    return templateMatch[4];
+  }
+
+  /**
+   * Get a list of templates
+   * @param {String} type Template type [viewTemplate|shelfTemplate]
+   * @param {Boolean} [isSub=false] Is subTemplate
+   */
+  async getTemplates(type, isSub = false) {
+    this.getTemplates.cache = this.getTemplates.cache || {};
+    const key = `${type}-${isSub}`;
+    const endpoint = `admin/a/PortalManagement/GetTemplateList?type=${type}&IsSub=${isSub ? 1 : 0}`;
+
+    this.api.interceptors.request.use((config) => {
+      config.headers.post['Cookie'] = `VtexIdclientAutCookie=${this.authCookie};`; // eslint-disable-line
+      config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'; // eslint-disable-line
+      return config;
+    });
+
+    if (!this.getTemplates.cache[key]) {
+      const { data } = await this.api.post(endpoint);
+      this.getTemplates.cache[key] = data;
+    }
+
+    return this.getTemplates.cache[key];
   }
 }
 
--- src/upload.js
+++ src/upload.js
@@ -1,11 +1,20 @@
 const inquirer = require('inquirer');
-// const Ora = require('ora');
+const Ora = require('ora');
 const clc = require('cli-color');
 
-// const vtexCMS = require('./VtexCMS');
 const { filesGlobMatch, getCurrentActive } = require('./utils/file');
 const { validateDiff } = require('./utils/validate');
 const { uploadQuestions } = require('./questions/upload');
+
+const vtexCMS = require('./VtexCMS');
+
+const spinner = new Ora({ color: 'yellow', indent: 2 });
+const cliExit = (content) => {
+  console.log();
+  console.log(content);
+  console.log();
+  process.exit(1);
+};
 
 inquirer.registerPrompt('path', require('inquirer-path').PathPrompt);
 
@@ -13,23 +22,46 @@ module.exports = async () => {
   const { type, files, confirm } = await inquirer.prompt(uploadQuestions);
 
   if (!confirm) {
-    console.log();
-    console.log(clc.red('Action cancelled'));
-    console.log();
-    process.exit(1);
+    cliExit(clc.red('Action cancelled'));
   }
 
   const current = getCurrentActive();
   const validate = validateDiff(current.updatedAt);
+  const { account, authCookie } = current;
 
   if (!validate) {
-    console.log();
-    console.log(`Session expired. Please login with ${clc.green('vtexify login')}`);
-    console.log();
-    process.exit(1);
+    cliExit(`Session expired. Please login with ${clc.green('vtexify login')}`);
   }
 
   // Process to upload
+  vtexCMS.setAccount(account, authCookie);
+
+  try {
+    spinner.start();
+    spinner.text = 'Generate token, please wait...';
+    await vtexCMS.getRequestToken();
+    spinner.succeed();
+    console.log();
+  } catch (err) {
+    cliExit(clc.red('Fail to generate Token. Please try again'));
+  }
+
+  try {
+    spinner.start();
+    spinner.text = 'Getting Template ID, please wait...';
+    const template = await vtexCMS.getLegacyTemplateId('account', 'viewTemplate', false);
+    spinner.succeed();
+
+    console.log();
+    console.log(template);
+    console.log();
+  } catch (err) {
+    cliExit(clc.red(err.message));
+  }
+
+  console.log();
+  console.log('REQUEST_TOKEN', vtexCMS.requestToken);
+  console.log();
   console.log(clc.green('PASSED'), type, files, confirm);
   console.log(filesGlobMatch(files));
 };
